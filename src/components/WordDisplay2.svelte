b650ce517eb5c023bbf26567f071ac342d44b390
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-03-13 23:09:06 -0500

    word formatting

--- src/components/WordDisplay2.svelte
+++ src/components/WordDisplay2.svelte
@@ -16,19 +16,22 @@
 		word = lexicon2.find((entry) => entry.id === wordId);
 		nextWord = nextWordId !== undefined ? lexicon2.find((entry) => entry.id === nextWordId) : undefined;
 		prevWord = prevWordId !== undefined ? lexicon2.find((entry) => entry.id === prevWordId) : undefined;
-		displayWord = typeof wordId === 'string' ? wordId : word?.orthography ?? '';
+		displayWord = typeof wordId === 'string' ? wordId : word?.orthography ?? `!!!could not find word ${wordId}!!!`;
 		const nextDisplayWord = typeof nextWordId === 'string' ? nextWordId : nextWord?.orthography ?? '';
 
-		let nextPunct = nextWord?.pos.includes('punct') || [',', '.', '!', '?', ':'].includes(String(nextWordId));
+		let nextPunct = nextWord?.pos.includes('punct') || [',', '.', '!', '?', ':', ')'].includes(String(nextWordId));
 		let prevPunct = prevWord?.pos.includes('punct');
 		let nextPunctRight = nextWord?.pos.includes('punct-right');
-		let thisPunctLeft = word?.pos.includes('punct-left');
+		let thisPunctLeft = word?.pos.includes('punct-left') || ['('].includes(String(wordId));
 		let isNum = /[0-9]/.test(displayWord);
 		let isNextNum = /[0-9]/.test(nextDisplayWord);
 		let betweenNums = isNum && isNextNum;
 
 		spaceAfter = true;
 		if (nextPunct || nextPunctRight || thisPunctLeft || betweenNums) {
+			spaceAfter = false;
+		}
+		if (displayWord === ':' && isNextNum) {
 			spaceAfter = false;
 		}
 	}
